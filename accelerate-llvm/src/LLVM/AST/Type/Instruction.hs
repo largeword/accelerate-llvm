65896bddc545ce7de6b806d4fab1f4a9be7a7eb2
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2023-09-04 13:39:58 +0200

    Fix codegen for GetElementPtr

--- accelerate-llvm/src/LLVM/AST/Type/Instruction.hs
+++ accelerate-llvm/src/LLVM/AST/Type/Instruction.hs
@@ -430,10 +430,13 @@ instance Downcast (Instruction a) LLVM.Instruction where
     LoadPtr v p           -> LLVM.Load (downcast v) (downcast $ typeOf p) (downcast p) atomicity alignment md
     GetElementPtr t n i   -> LLVM.GetElementPtr inbounds (downcast t) (downcast n) (downcast i) md
     GetStructElementPtr _ n i -> case typeOf n of
-      t@(PrimType (PtrPrimType (StructPrimType _ tp) _)) ->
+      (PrimType (PtrPrimType t@(StructPrimType _ tp) _)) ->
                              LLVM.GetElementPtr inbounds (downcast t) (downcast n) [constant (0 :: Int), constant (fromIntegral $ tupleIdxToInt tp i :: Int32)] md
       _ -> internalError "Struct ptr impossible"
-    GetVecElementPtr _ n i -> LLVM.GetElementPtr inbounds (downcast $ typeOf n) (downcast n) [constant (0 :: Int), downcast i] md
+    GetVecElementPtr _ n i -> case typeOf n of
+      (PrimType (PtrPrimType t _)) ->
+        LLVM.GetElementPtr inbounds (downcast t) (downcast n) [constant (0 :: Int), downcast i] md
+      _ -> internalError "Ptr impossible"
 #else
     Load _ v p            -> LLVM.Load (downcast v) (downcast p) atomicity alignment md
     LoadPtr v p           -> LLVM.Load (downcast v) (downcast p) atomicity alignment md
